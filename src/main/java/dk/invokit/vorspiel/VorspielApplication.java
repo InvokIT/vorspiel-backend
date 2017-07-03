8b95ce534f03e5b644d1e8df6d318a3974fc39bc
Sebastian Jørgensen <sj@invokit.dk>
2017-07-03 19:44:19 +0100

    Generated from Spring Initializr

---
+++ src/main/java/dk/invokit/vorspiel/VorspielApplication.java
@@ -0,0 +1,12 @@
+package dk.invokit.vorspiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorspielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorspielApplication.class, args);
+	}
+}
